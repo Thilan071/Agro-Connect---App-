bda85ad977d27b6f86fd4e14d63d71caf6f76304
Thilan071 <thilantheekshana221@gamil.com>
2025-01-23 02:51:50 +0530

    deleted unnessery code lines

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: false,
-//       ),
-//       home: const Scaffold(
-//         body: Center(
-//           child: Text(', World!'),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 
 void main() {
